4e8a5750c8b22aef1c3ab528125e386b35213aa3
vanArthur <vanackere.arthur@gmail.com>
2026-01-22 13:15:56 +0100

    initialize raylib window

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
-#include <stdio.h>
+#include <raylib.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+#define WIDTH 1000
+#define HEIGHT 1000
+
+
+int main(void) {
+  InitWindow(WIDTH, HEIGHT, "Advanced Character Physics");
+  SetTargetFPS(60);
+
+  int w = GetScreenWidth();
+  int h = GetScreenHeight();
+
+  while (!WindowShouldClose()) {
+    BeginDrawing();
+    ClearBackground(GetColor(0x052A4Fff));
+    EndDrawing();
+  }
+
+  CloseWindow();
+  return 0;
 }
